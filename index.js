66c790a22fc550abd24cb905cd8323af778ac6e5
matheus <matheus.lindo@email.com>
2020-05-26 17:13:26 -0300

    Outro endpoint

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/matheus', (req, res) => {
+    res.send('O Matheus é muito lindo!');
+});
+
 app.listen(8080, () => console.log('Ouvindo na 8080'));
